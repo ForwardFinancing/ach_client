74adf217b542707629ec0ddd16809a6f2adda290
zcotter@forwardfinancing.com <jaffecotter.z@gmail.com>
2016-08-19 11:23:56 -0400

    Fix requires in initializer

--- lib/ach_client.rb
+++ lib/ach_client.rb
@@ -3,7 +3,9 @@ require 'savon'
 require 'sucker_punch'
 
 # Require all of the files in lib
-Dir['./lib/**/*.rb'].sort.each { |f| require(f) }
+Dir['../ach_client/lib/**/*.rb'].sort.each do |f|
+  require(f.split('../ach_client/lib/').last.split('.rb').first)
+end
 
 # Adapter for interacting with various Ach service providers
 module AchClient
