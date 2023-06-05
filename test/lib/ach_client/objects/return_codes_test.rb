e494f781ec255d259fb653081e3f7dc532520f93
thomas-scoggins <102810364+thomas-scoggins@users.noreply.github.com>
2023-06-05 11:24:42 -0400

    Update test/lib/ach_client/objects/return_codes_test.rb
    Co-authored-by: James Crockett <jcrockett@forwardfinancing.com>

--- test/lib/ach_client/objects/return_codes_test.rb
+++ test/lib/ach_client/objects/return_codes_test.rb
@@ -3,11 +3,11 @@ require 'test_helper'
 class ReturnCodesTest < MiniTest::Test
   def test_unauthorized
     unauthorized_codes = AchClient::ReturnCodes.unauthorized
-    assert(unauthorized_codes.all?(&:unauthorized_return?)
+    assert(unauthorized_codes.all?(&:unauthorized_return?))
   end
 
   def test_administrative
     administrative_codes = AchClient::ReturnCodes.administrative
-    assert(administrative_codes.all?{|code| code.administrative_return?})
+    assert(administrative_codes.all?(&:administrative_return?))
   end
 end
