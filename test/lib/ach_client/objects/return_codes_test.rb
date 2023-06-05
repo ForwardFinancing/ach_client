76909c3f2f25dc2a8bd51b3f7fa2b4380d0eb1b1
thomas-scoggins <102810364+thomas-scoggins@users.noreply.github.com>
2023-06-05 11:33:00 -0400

    adding methods to get specific return types (#70)
    * adding methods to get specific return types
    
    * Update test/lib/ach_client/objects/return_codes_test.rb
    
    Co-authored-by: James Crockett <jcrockett@forwardfinancing.com>
    
    * Update test/lib/ach_client/objects/return_codes_test.rb
    
    Co-authored-by: James Crockett <jcrockett@forwardfinancing.com>
    
    ---------
    
    Co-authored-by: James Crockett <jcrockett@forwardfinancing.com>

---
+++ test/lib/ach_client/objects/return_codes_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class ReturnCodesTest < MiniTest::Test
+  def test_unauthorized
+    unauthorized_codes = AchClient::ReturnCodes.unauthorized
+    assert(unauthorized_codes.all?(&:unauthorized_return?))
+  end
+
+  def test_administrative
+    administrative_codes = AchClient::ReturnCodes.administrative
+    assert(administrative_codes.all?(&:administrative_return?))
+  end
+end
